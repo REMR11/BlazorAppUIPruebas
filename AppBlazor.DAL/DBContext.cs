2b47bdaeee0b9b84e0d8b000237aa6dc24385027
REMR11 <remr11@outlook.com>
2022-09-14 20:14:00 -0600

    V1.2 Practicas Blazor

---
+++ AppBlazor.DAL/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppBlazor.EL;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppBlazor.DAL
+{
+    public class DBContext:DbContext
+    {
+        public DbSet<Category> Category { get; set; }
+        //public DbSet<Expenses> Expenses { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=BlazorApp;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        }
+
+    }
+}
---
+++ AppBlazor.DAL/Documents/DBBlazorApp.cs
@@ -0,0 +1,26 @@
+﻿//DROP DATABASE BlazorApp
+//GO
+
+//CREATE DATABASE BlazorApp
+//GO
+
+//CREATE TABLE Category(
+//	IdCategory INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+//	NameCategory NVARCHAR(50)
+//)
+//GO
+
+//CREATE TABLE Expenses(
+//	IdExpen INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+//	Amount DECIMAL(18,0) NOT NULL,
+//	IdCategory INT NOT NULL,
+//	ExpenseType NVARCHAR(30),
+//	TransactionDate Date NOT NULL
+//)
+//GO
+
+//ALTER TABLE Expenses
+//ADD CONSTRAINT FK_EX_CATE
+//FOREIGN KEY (IdCategory)
+//REFERENCES Category(IdCategory)
+//GO
